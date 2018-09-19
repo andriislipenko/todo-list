2cce58148d339933ff9921151894238afdf6684d
andrewsHP <andrii.slipenko@gmail.com>
2018-09-19 11:36:01 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    TodoModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
