4bbd4abaea673b65e645f34ff0d209fd77c2bc98
Anri5 <chdtu13@gmail.com>
2018-09-20 07:51:45 +0300

    OOH 09/20

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
 import {CheckboxModule} from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
     CheckboxModule,
     FormsModule,
-    ConfirmDialogModule
+    ConfirmDialogModule,
+    ButtonModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     TodoListComponent
