2a79cd1c05289f3500e7c3286dc3a3868c3c9525
andrewsHP <andrii.slipenko@gmail.com>
2018-10-05 19:00:36 +0300

    OOF 10/05

--- src/app/weather/weather-date.pipe.ts
+++ src/app/weather/weather-date.pipe.ts
@@ -6,10 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class WeatherDatePipe implements PipeTransform {
 
     transform(value: string): string {
-        return new Date(+value * 1000).toLocaleString('en', {
+        return new Date(+value * 1000).toLocaleString('uk', {
+            hour: '2-digit',
+            minute: '2-digit',
             day: '2-digit',
-            month: '2-digit',
-            year: '2-digit'
+            month: '2-digit'
         });
       }
 
